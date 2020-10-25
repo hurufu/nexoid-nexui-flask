29c3d20046b14856ab41efeac6f4c3c5f17f625e
Aleksy Grabowski <hurufu@gmail.com>
2020-10-25 22:51:46 +0100

    Use separate thread for req handling

--- test.py
+++ test.py
@@ -1,14 +1,10 @@
 import response_socket as rs
 import notification_socket as ns
+import threading
+from time import sleep
 
-def main():
-    with rs.ResponseSocket(listen='tcp://0.0.0.0:50153', recv_timeout=3 * 1000) as s, ns.NotificationSocket(listen='tcp://0.0.0.0:50154') as n:
-        evt = b'<ScapiNotification><events><languageSelection><language>pl</language>'\
-              b'</languageSelection><serviceSelection><serviceId><cardValidityCheck/>'\
-              b'</serviceId></serviceSelection></events></ScapiNotification>'
-        print('ntf: ' + evt.decode(encoding='UTF-8'))
-        n.send(evt)
-
+def handle_scapi_requests(**kwargs):
+    with rs.ResponseSocket(**kwargs) as s:
         while True:
             req = s.recv()
             print('req: ' + req.decode(encoding='UTF-8'))
@@ -16,5 +12,22 @@ def main():
             rsp = b'<ScapiResponse><ack/></ScapiResponse>'
             print('rsp: ' + rsp.decode(encoding='UTF-8'))
             s.send(rsp)
+
+def start_req_handler():
+    req_params = {
+        'listen': 'tcp://0.0.0.0:50153'
+    }
+    req_thread = threading.Thread(target=handle_scapi_requests, name='reqhndlr', daemon=True, kwargs=req_params)
+    req_thread.start()
+
+def main():
+    start_req_handler()
+    with ns.NotificationSocket(listen='tcp://0.0.0.0:50154') as n:
+        evt = b'<ScapiNotification><events><languageSelection><language>pl</language>'\
+              b'</languageSelection><serviceSelection><serviceId><cardValidityCheck/>'\
+              b'</serviceId></serviceSelection></events></ScapiNotification>'
+        print('ntf: ' + evt.decode(encoding='UTF-8'))
+        n.send(evt)
+        sleep(5)
 
 main()
