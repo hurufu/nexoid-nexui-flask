33e56acb7bcdae33465648f9c2c2a45fd1968526
Aleksy Grabowski <hurufu@gmail.com>
2020-10-25 21:26:06 +0100

    Simplify test.py

--- test.py
+++ test.py
@@ -1,38 +1,20 @@
 import response_socket as rs
 import notification_socket as ns
 
-with rs.ResponseSocket(listen='tcp://0.0.0.0:50153') as s:
-    with ns.NotificationSocket(listen='tcp://0.0.0.0:50154') as n:
-        req = s.recv()
-        print('> ' + req.decode(encoding='UTF-8'))
-
-        rsp = b'<ScapiResponse><ack/></ScapiResponse>'
-        print('< ' + rsp.decode(encoding='UTF-8'))
-        s.send(rsp)
-
-        req = s.recv()
-        print('> ' + req.decode(encoding='UTF-8'))
-
-        rsp = b'<ScapiResponse><ack/></ScapiResponse>'
-        print('< ' + rsp.decode(encoding='UTF-8'))
-        s.send(rsp)
-
+def main():
+    with rs.ResponseSocket(listen='tcp://0.0.0.0:50153', recv_timeout=3 * 1000) as s, ns.NotificationSocket(listen='tcp://0.0.0.0:50154') as n:
         evt = b'<ScapiNotification><events><languageSelection><language>pl</language>'\
               b'</languageSelection><serviceSelection><serviceId><cardValidityCheck/>'\
               b'</serviceId></serviceSelection></events></ScapiNotification>'
-        print('< ' + evt.decode(encoding='UTF-8'))
+        print('ntf: ' + evt.decode(encoding='UTF-8'))
         n.send(evt)
 
-        req = s.recv()
-        print('> ' + req.decode(encoding='UTF-8'))
+        while True:
+            req = s.recv()
+            print('req: ' + req.decode(encoding='UTF-8'))
 
-        rsp = b'<ScapiResponse><ack/></ScapiResponse>'
-        print('< ' + rsp.decode(encoding='UTF-8'))
-        s.send(rsp)
+            rsp = b'<ScapiResponse><ack/></ScapiResponse>'
+            print('rsp: ' + rsp.decode(encoding='UTF-8'))
+            s.send(rsp)
 
-        req = s.recv()
-        print('> ' + req.decode(encoding='UTF-8'))
-
-        rsp = b'<ScapiResponse><ack/></ScapiResponse>'
-        print('< ' + rsp.decode(encoding='UTF-8'))
-        s.send(rsp)
+main()
