7bf9cc7ef8177f5e25fc7a7e51a2b5b19d43f1a1
Aleksy Grabowski <hurufu@gmail.com>
2020-10-31 12:26:24 +0100

    Fix all pylint warnings (default settings)

--- nexui/blueprint_ui.py
+++ nexui/blueprint_ui.py
@@ -1,25 +1,36 @@
+'''SSE UI updates handler
+'''
 import click
 from flask import (
     Blueprint,
     request,
     Response,
 )
+import posix_ipc as ipc
 from . import message_queue as m
 
 bp = Blueprint('pay', __name__, url_prefix='/pay')
 
 @bp.route('/display', methods=['GET'])
 def handle_display_update():
+    '''SSE entry point
+    '''
     if request.headers.get('accept') != 'text/event-stream':
-        return Response('This endpoint is meant only for event stream listeners', mimetype='text/plain')
+        rsp = 'This endpoint is meant only for event stream listeners'
+        return Response(rsp, mimetype='text/plain')
 
     def get_update_event():
-        with m.MessageQueue('/nexoid:v1:display', flags = m.O_CREAT, read = True, write = False) as mq:
+        mqkw = {
+            'flags': ipc.O_CREAT,
+            'read': True,
+            'write': False
+        }
+        with m.MessageQueue('/nexoid:v1:display', **mqkw) as msq:
             while True:
-                (buf, priority) = mq.receive()
+                (buf, priority) = msq.receive()
                 ret = 'data: ' + buf.decode(encoding='UTF-8') + '\n\n'
                 click.echo('Forwarding msg with priority: ' + str(priority) + '\n' + ret)
                 yield ret
 
-    r = get_update_event()
-    return Response(r, mimetype='text/event-stream')
+    evt = get_update_event()
+    return Response(evt, mimetype='text/event-stream')
--- nexui/message_queue.py
+++ nexui/message_queue.py
@@ -1,14 +1,23 @@
+'''POSIX IPC wrapper
+'''
 import posix_ipc
-
-O_CREAT = posix_ipc.O_CREAT
+import click
 
 class MessageQueue:
+    '''Simple context manager for POSIX MQ
+    '''
 
     def __init__(self, *args, **kwargs):
         self.queue = posix_ipc.MessageQueue(*args, **kwargs)
+        self.name = args[0]
+        debug_msg = 'Queue "' + self.name + '" opened'
+        click.echo(debug_msg)
 
     def __enter__(self):
         return self.queue
 
-    def __exit__(self, type, value, traceback):
+    def __exit__(self, exception_type, exception_value, traceback):
+        debug_msg = 'Queue "' + self.name + '" closed, '\
+                  + 'type: ' + str(exception_type) + ' ' + str(exception_value)
+        click.echo(debug_msg)
         self.queue.close()
