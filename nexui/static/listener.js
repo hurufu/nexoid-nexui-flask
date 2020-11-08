b66bb8dd6725a6a99750a032c70310c9ebbd70b2
Aleksy Grabowski <hurufu@gmail.com>
2020-11-08 13:25:55 +0100

    Dispatch UI events to xforms

--- nexui/static/listener.js
+++ nexui/static/listener.js
@@ -11,11 +11,16 @@ var dec = new TextDecoder('utf-8');
 var enc = new TextEncoder();
 
 sock.on('data', function(msg) {
+    function get_header() {
+        return Array.from(new Uint8Array(msg)).slice(0, 4);
+    }
+    function make_id() {
+        return get_header().map(function(x){return x.toString(16);}).join('');
+    }
     function make_response(str) {
         var txt = Array.from(enc.encode(str));
-        var hdr = Array.from(new Uint8Array(msg)).slice(0, 4);
-        var rsp = new Uint8Array(hdr.concat(txt));
-        return rsp;
+        return new Uint8Array(get_header().concat(txt));
     }
+    xf_fireEvent("request_log_model", "ui_request", {id: make_id()});
     sock.send(make_response('<ScapiResponse><ack/></ScapiResponse>'));
 });
