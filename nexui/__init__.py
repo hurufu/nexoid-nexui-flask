3638931fa0c593f8d9df1e5d2605812fb107297f
Aleksy Grabowski <hurufu@gmail.com>
2021-01-15 19:12:26 +0100

    Add useful logging facility + remove rarely used py files

--- nexui/__init__.py
+++ nexui/__init__.py
@@ -1,6 +1,12 @@
 '''NEXUI - Demo UI for nexo-in-the-cloud'''
 import threading
 import subprocess
+from logging.config import dictConfig
+from logging import (
+    info,
+    debug,
+    warning,
+)
 from time import sleep
 from contextlib import contextmanager
 from traceback import print_exc
@@ -15,15 +21,55 @@ import click
 
 import pynng
 from timebudget import timebudget
-from . import response_socket as rs
-from . import notification_socket as ns
 from . import scap4nexui
 
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s %(levelname).1s %(threadName)-13s %(message)s',
+        }
+    },
+    'handlers': {
+        'wsgi': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://flask.logging.wsgi_errors_stream',
+            'formatter': 'default'
+        }
+    },
+    'root': {
+        'level': 'DEBUG',
+        'handlers': [
+            'wsgi'
+        ]
+    }
+})
+
 @contextmanager
-def survey_socket(*args, **kwargs):
-    '''NNG Surveyor0 context manager'''
-    socket = pynng.Surveyor0(*args, **kwargs)
+def browser_distributor(*args, name='browser_distributor', **kwargs):
+    '''Creates a socket for distributing requests to all connected browsers'''
+    socket = pynng.Surveyor0(*args, name=name, **kwargs)
+    info(f"{socket.protocol_name} '{name}' is listening on {kwargs['listen']}")
     yield socket
+    info(f"{socket.protocol_name} '{name}' at {kwargs['listen']} is stopped")
+    socket.close()
+
+@contextmanager
+def local_ui_requests_gatherer(*args, name='ui_gatherer', **kwargs):
+    '''Creates a socket context manager for gathering UI requests from local peers'''
+    socket = pynng.Rep0(*args, **kwargs)
+    info(f"{socket.protocol_name} '{name}' is listening on {kwargs['listen']}")
+    yield socket
+    info(f"{socket.protocol_name} '{name}' at {kwargs['listen']} is stopped")
+    socket.close()
+
+@contextmanager
+def notification_socket(*args, name='fat_notifier', **kwargs):
+    '''Creates a socket for FAT notifications'''
+    socket = pynng.Push0(*args, **kwargs)
+    info(f"{socket.protocol_name} '{name}' dialed to {kwargs['dial']}")
+    yield socket
+    info(f"{socket.protocol_name} '{name}' dropped connection to {kwargs['dial']}")
     socket.close()
 
 app = Flask(__name__, instance_relative_config=True)
@@ -36,8 +82,8 @@ def get_scap_notification_form():
 @app.route('/nexo', methods=['POST'])
 def notify_scap():
     '''Forward SCAPI notifications directly to the FAT'''
-    with ns.Socket(dial='ipc:///tmp/fatnt') as ntf:
-        click.echo(request.data)
+    with notification_socket(dial='ipc:///tmp/fatnt') as ntf:
+        debug(f"Notify SCAP s:/tmp/fatnt n:{ntf.name} p:{ntf.peer_name} {str(request.data)}")
         ntf.send(request.data)
     return redirect(url_for('static', filename='notification.xhtml'))
 
@@ -48,23 +94,27 @@ def start_ui_server():
     def forward_ui_requests(**kwargs):
         '''Blindly forwards all UI requests to a web browser'''
         sleep(15)
-        with rs.Socket(**kwargs['nexui']) as nexui,\
-             survey_socket(**kwargs['browser']) as browser:
+        with local_ui_requests_gatherer(**kwargs['nexui']) as nexui,\
+             browser_distributor(**kwargs['browser']) as browser:
             sleep(2)
             while True:
                 try:
                     req = nexui.recv()
+                    debug(f"Received UI request {req}")
                     with timebudget("Browser roundtrip", quiet=kwargs['quiet_time']):
                         browser.send(req)
                         rsp = browser.recv()
                     nexui.send(rsp)
+                    debug(f"UI response {rsp}")
                 except pynng.exceptions.Timeout:
                     print_exc()
 
     def run_external_program(**kwargs):
         sleep(22)
         while True:
+            info('Nexoid process started')
             subprocess.run(kwargs['prog'], check=False)
+            warning('Nexoid process ended')
             sleep(2)
 
     def run_scap4nexui():
--- nexui/scap4nexui/__init__.py
+++ nexui/scap4nexui/__init__.py
@@ -1,35 +1,43 @@
 '''Basic SCAP Application'''
 
 from contextlib import contextmanager
-from datetime import datetime as dt
+from logging import (
+    info,
+    debug,
+)
+
 from pynng import (Req0, Rep0)
 from . import scapi_message as sm
 
 @contextmanager
-def request_socket(*args, **kwargs):
-    '''NNG Req0 context manager'''
+def ui_socket(*args, name='ui_req_from_scap', **kwargs):
+    '''Creates UI socket for forwarding converted SCAPI requests'''
     socket = Req0(*args, **kwargs)
+    info(f"{socket.protocol_name} '{name}' dialed to {kwargs['dial']}")
     yield socket
+    info(f"{socket.protocol_name} '{name}' dropped connection to {kwargs['dial']}")
     socket.close()
 
 @contextmanager
-def response_socket(*args, **kwargs):
-    '''NNG Rep0 context manager'''
+def scapi_endpoint(*args, name='scapi_endpoint', **kwargs):
+    '''Creates SCAPI endpoint socket'''
     socket = Rep0(*args, **kwargs)
+    info(f"{socket.protocol_name} '{name}' is listening on {kwargs['listen']}")
     yield socket
+    info(f"{socket.protocol_name} '{name}' at {kwargs['listen']} is stopped")
     socket.close()
 
 def main():
     '''Main request forwarding loop'''
-    with response_socket(listen='ipc:///tmp/fatrq') as fat,\
-         request_socket(dial='ipc:///tmp/nexui') as nexui:
+    with scapi_endpoint(listen='ipc:///tmp/fatrq') as fat,\
+         ui_socket(dial='ipc:///tmp/nexui') as nexui:
         def exchange_messages():
-            req = sm.tonexui(fat.recv())
-            nexui.send(req)
-            print(dt.now(), ' req: ' + req.decode('UTF-8'))
-            rsp = nexui.recv()
-            fat.send(sm.fromnexui(rsp))
-            print(dt.now(), ' rsp: ' + rsp.decode('UTF-8'))
+            req = fat.recv()
+            debug(f"Received SCAPI request {req}")
+            nexui.send(sm.tonexui(req))
+            rsp = sm.fromnexui(nexui.recv())
+            fat.send(rsp)
+            debug(f"SCAPI response {rsp}")
 
         while True:
             exchange_messages()
