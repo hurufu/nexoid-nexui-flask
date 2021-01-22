468482581a3b345b934253c21dd75cf4564bac29
Aleksy Grabowski <hurufu@gmail.com>
2021-01-22 17:48:43 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+'''setup.py'''
+import setuptools
+
+setuptools.setup(
+    name="nexui-demo",
+    version="0.0.1",
+    author="Aleksy Grabowski",
+    author_email="huruf@gmail.com",
+    description="Nexo-in-the-cloud demo",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
